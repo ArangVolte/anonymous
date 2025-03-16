fb3bff1718d68958ca196db53e224873a998d144
xx <belajarsendiri474@gmail.com>
2025-03-16 11:53:27 +0700

    update anon.py

--- anon.py
+++ anon.py
@@ -7,8 +7,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 # Konfigurasi API
 API_ID = int(getenv("API_ID", "15370078"))
-API_HASH = getenv("API_HASH", "e5e8756e459f5da3645d35862808cb30"))
-BOT_TOKEN = getenv("BOT_TOKEN", "6208650102:AAF6CyhFQk8b-duLd44A67chU_cHXlX9SOQ"))
+API_HASH = getenv("API_HASH", "e5e8756e459f5da3645d35862808cb30")
+BOT_TOKEN = getenv("BOT_TOKEN", "6208650102:AAF6CyhFQk8b-duLd44A67chU_cHXlX9SOQ")
 
 # Inisialisasi bot
 app = Client("anonim_chatbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
