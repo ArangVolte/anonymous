8a75a8c46c1870a5624f317e23798558fdcfe21c
xx <belajarsendiri474@gmail.com>
2025-03-16 11:19:07 +0700

    update anon.py

--- anon.py
+++ anon.py
@@ -150,7 +150,7 @@ async def handle_message(client, message: Message):
         await message.reply_text(get_message(user_id, "error_message"))
         return
 
-    reply_to_id = message.reply_to_message.message.id if message.reply_to_message else None
+    reply_to_id = message.reply_to_message.message.id if message.reply_to_message.id else None
 
     try:
         if message.text:
