c957abc94d804bcfc65d0cfd069fa25db948a561
xx <belajarsendiri474@gmail.com>
2025-03-17 17:36:26 +0700

    update anon.py

--- anon.py
+++ anon.py
@@ -187,7 +187,7 @@ async def handle_callback(client, callback_query):
     mid = InputMediaPhoto(xx)
     await app.edit_message_media(
         chat_id=callback_query.from_user.id,
-        message_id=int(ms),
+        message_id=int(ms+1),
         media=mid
     )
 # Jalankan bot
