49cd5b29eeb0e32dfce1e57dbad46bfa72c2992d
xx <belajarsendiri474@gmail.com>
2025-03-19 02:44:11 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+
 from anon import app
 from pyrogram.types import BotCommand
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     print("Bot sudah aktif")
     try:
-        app.start()
+        app.run()
         app.set_bot_commands([
             BotCommand("start", "Memulai bot"),
             BotCommand("next", "Mencari pasangan chat"),
@@ -13,7 +16,5 @@ if __name__ == '__main__':
             BotCommand("help", "Menampilkan pesan bantuan"),
             BotCommand("settings", "Ubah jenis kelamin dan pengaturan lainnya")
         ])
-        print("Perintah bot berhasil dipasang.")
-        
     except Exception as e:
         print(f"Bot mengalami error: {e}")
