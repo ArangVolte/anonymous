6065597465e376a1dd2b2051b6cb9c21261f274a
xx <belajarsendiri474@gmail.com>
2025-03-19 02:40:00 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from anon import app
 from pyrogram.types import BotCommand
-import logging
-import asyncio
 
-logging.basicConfig(level=logging.INFO)
 
-async def main():
-    print("Bot sudah aktif")
+if __name__ == '__main__':
+print("Bot sudah aktif")
     try:
-        # Mulai bot
-        await app.start()
-        
-        # Set perintah bot
-        await app.set_bot_commands([
+        app.start()
+        app.set_bot_commands([
             BotCommand("start", "Memulai bot"),
             BotCommand("next", "Mencari pasangan chat"),
             BotCommand("stop", "Menghentikan chat"),
@@ -23,10 +17,3 @@ async def main():
         
     except Exception as e:
         print(f"Bot mengalami error: {e}")
-    finally:
-        # Hentikan bot saat program selesai
-        await app.stop()
-
-if __name__ == '__main__':
-    # Jalankan fungsi main secara asinkron
-    asyncio.run(main())
