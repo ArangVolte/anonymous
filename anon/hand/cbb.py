470c32a51f3de0aaefbb2fabd1c5f5664ab38622
xx <belajarsendiri474@gmail.com>
2025-03-19 11:00:34 +0700

    update cbb.py

--- anon/hand/cbb.py
+++ anon/hand/cbb.py
@@ -120,6 +120,7 @@ async def next_page(client, callback_query):
             [InlineKeyboardButton("❌ Hapus Usia", callback_data="remove_age")],
             [InlineKeyboardButton("← Kembali", callback_data="back_to_main")]
         ]
+        ]
     )
     await callback_query.edit_message_reply_markup(reply_markup=keyboard)
 
